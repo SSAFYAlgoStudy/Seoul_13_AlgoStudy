c68c1b9819d2cc11bb1ef52a4c7f0be4ad88dc2d
limjunsung225 <jsbryan@naver.com>
2023-02-27 21:27:16 +0900

    벽뿌2 3주차

--- LJS/2주차모임/벽 부수고 이동하기2.py
+++ LJS/2주차모임/벽 부수고 이동하기2.py
@@ -1,49 +1,38 @@
 import sys
-input= sys.stdin.readline
+input = sys.stdin.readline
 from collections import deque
-sys.setrecursionlimit(10**7)
 
-N , M = map(int,input().split())
 
-graph = [ [] for _ in range(N) ]
-dx = [-1,1,0,0]
-dy=[0,0,-1,1]
+def bfs() :
+    
+    q.append([0,0,K]) 
+    visited[0][0][K] = 1 
 
-q = deque([])
-q.append([0,0,0])
-result_cnt = [[ [0] * 2 for _ in range(M) ] for _ in range(N) ]  # 3차원 배열 result_cnt 생성 flag를 저장하기위해 만들어주고
-result_cnt[0][0][0] = 1 #시작할 때 1 
-
-for i in range(N):
-    graph[i] = (list(map(int,input().rstrip())))
-
-def bfs():
-
-    while q:
-        x , y , flag = q.popleft()  # xpos ,  ypos, 결과값
-
+    while q :
+        x,y,flag = q.popleft()   # xpos ,  ypos, 결과값
         if (x==N-1 and y==M-1):
-            return result_cnt[x][y][flag]
-
-        for i in range(4):
+            return visited[x][y][flag]
+        
+        for i in range(4) :
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx<=-1 or nx >= N or ny <= -1 or ny>=M: # 맵밖으로 나가면 
-                continue
-            
-            if graph[nx][ny] == 1 and flag == 0:  # 벽에 막혀있는데 아직 안부쉈으면
-                result_cnt[nx][ny][1] += (result_cnt[x][y][0]+1) # 이전 결과값에서 1 증가시켜주고
-                q.append([nx,ny,1])
+            if 0<=nx<N and 0<=ny<M :  # 맵밖으로 나가지 않으면 
 
-            # if graph[nx][ny] == 1 and flag != 0: # 벽에 막혀있으며 부쉈으면 
-            #     continue
+                if flag > 0 and graph[nx][ny]==1 and visited[nx][ny][flag-1]==0:
+                    visited[nx][ny][flag-1] = visited[x][y][flag]+1
+                    q.append([nx,ny,flag-1])
 
-            if graph[nx][ny] == 0 and result_cnt[nx][ny][flag] == 0: # 벽이 아니면서, 새로 방문하는 곳이면
-                result_cnt[nx][ny][flag] = result_cnt[x][y][flag] + 1 # 거리를 1 증가시키며 q에 저장한다.
-                q.append([nx,ny,flag])
+                elif graph[nx][ny]==0 and visited[nx][ny][flag]==0:  # 벽이 아니면서, 새로 방문하는 곳이면
+                    visited[nx][ny][flag] = visited[x][y][flag]+1  # 거리를 1 증가시키며 q에 저장한다.
+                    q.append([nx,ny,flag]) 
+    return -1
 
-    return -1  # 못돌면 -1출력한다.
-
+N, M , K= map(int, input().split())
+graph = [list(map(int,input().strip())) for _ in range(N)]
+visited = [[[0]*(K+1) for _ in range(M)] for _ in range(N)]  # 3차원 배열 result_cnt 생성 flag를 저장하기위해 만들어주고
+q = deque([])
+dx = [-1,1,0,0]
+dy=[0,0,-1,1]
 
 print(bfs())
