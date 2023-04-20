0f7cc5b54100da97686757fa810672a59be1c939
ljs <jsbryan@naver.com>
2023-04-20 12:33:39 +0900

    1655 16938

---
+++ LJS/6주차모임/16938.py
@@ -0,0 +1,13 @@
+import sys
+input =sys.stdin.readline
+import itertools
+N , L , R, X = map(int,input().split()) #  문제개수, L<=합M=R , 가장어려운문제 ,가장쉬운문제 난이도차이
+li = list(map(int,input().split())) # 문제의 난이도
+li.sort()
+result = 0
+
+for i in range(2,N+1):
+    for comb in itertools.combinations(li,i):
+        if L<=sum(comb)<=R and (max(comb)-min(comb))>=X:
+            result += 1
+print(result)
